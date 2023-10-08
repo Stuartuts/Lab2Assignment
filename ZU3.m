5b82bf10f85258ff66b350cbd39bef723b8c123c
Stuartuts <Stuart.Macdonald@student.uts.edu.au>
2023-10-09 03:26:41 +1100

    Update ZU3.m

--- ZU3.m
+++ ZU3.m
@@ -1,7 +1,7 @@
 classdef ZU3 < handle
     properties
         model;
-        workspace = [-5 5 -5 5 0 5]
+        workspace = [-5 5 -5 5 0 5];
     end
 
     methods
@@ -10,7 +10,7 @@ classdef ZU3 < handle
         end
 
         function SetZU3(self)
-            L(1) = Link('d',0.1506,'a',-0.4,'alpha',-pi/2,'offset',0,'qlim',[deg2rad(-270) deg2rad(270)]);
+            L(1) = Link('d',0.1506,'a',-0.4,'alpha',0,'offset',0,'qlim',[deg2rad(-270) deg2rad(270)]);
             L(2) = Link('d',-0.1150,'a',0.0,'alpha',deg2rad(-90),'offset',0,'qlim',[deg2rad(-85) deg2rad(265)]);
             L(3) = Link('d',0.1095,'a',0.2460,'alpha',pi/2,'offset',0,'qlim',[deg2rad(-175) deg2rad(175)]);
             L(4) = Link('d',-0.1175,'a',0.2280,'alpha',deg2rad(-90),'offset',0,'qlim',[deg2rad(-85) deg2rad(265)]);
@@ -18,7 +18,7 @@ classdef ZU3 < handle
             L(6) = Link('d',-0.1050,'a',0.0,'alpha',deg2rad(-90),'offset',0,'qlim',[deg2rad(-270) deg2rad(270)]);
             self.model = SerialLink(L,'name', 'ZU3');
             
-            q = [0 0 0 0 0 0];
+            q = [deg2rad(-90) deg2rad(-90) 0 deg2rad(90) 0 0];
             self.model.teach(q);
         end
 
