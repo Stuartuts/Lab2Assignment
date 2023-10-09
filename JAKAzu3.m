037f1434ba7fac0f1837df5fdbea6b5b4142da57
quocminh-tran-1 <126060189+quocminh-tran-1@users.noreply.github.com>
2023-10-09 18:36:21 +1100

    Add files via upload
    Updating DH parameter for Zu3

--- JAKAzu3.m
+++ JAKAzu3.m
@@ -19,25 +19,25 @@ classdef JAKAzu3 < RobotBaseClass
 
         function CreateModel(self)
             % Create the Zu3 robot model
-                link(1) = Link([pi    0.1506  -0.4000   0       pi/2   0]);                     % Joint 1
-                link(2) = Link([0     -0.1150  0        -pi/2   -90    -85*pi/180 265*pi/180]); % Joint 2
-                link(3) = Link([0      0.1095  0.2460   0       0     -175*pi/180]);            % Joint 3
-                link(4) = Link([0     -0.1175  0.2280   0       -90   -85*pi/180 265*pi/180]);  % Joint 4
-                link(5) = Link([0     -0.1175  0        0       90    -270*pi/180]);            % Joint 5
-                link(6) = Link([0     -0.1050  0        0       -90   -270*pi/180]);            % Joint 6
-    
+               
+                link(1) = Link([-pi/2    0.1506 -0.4000    0     0]); 
+                link(2) = Link([-pi/2   -0.1150  0       -pi/2   0]);
+                link(3) = Link([ 0       0.1095  0.2460    0     0]);
+                link(4) = Link([ pi/2   -0.1175  0.2280  -pi/2   0]);
+                link(5) = Link([ 0      -0.1175  0        pi/2   0]);
+                link(6) = Link([ 0      -0.1050  0       -pi/2   0]);
             % Incorporate joint limits
-                link(1).qlim = [-90 90]*pi/180;
-                link(2).qlim = [-90 90]*pi/180;
+                link(1).qlim = [-270 270]*pi/180;
+                link(2).qlim = [-85 265]*pi/180;
                 link(3).qlim = [-175 175]*pi/180;
-                link(4).qlim = [-90 90]*pi/180;
+                link(4).qlim = [-85 265]*pi/180;
                 link(5).qlim = [-270 270]*pi/180;
                 link(6).qlim = [-270 270]*pi/180;
     
             % Joint offsets
                 link(2).offset = -90*pi/180;
     
-                self.model = SerialLink(link, 'name', self.name);
+    self.model = SerialLink(link, 'name', self.name);
         end
     end
 end
