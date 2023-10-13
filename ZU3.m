7dc6793fd50195468d3d475f855c4bdf2faf8c27
Stuartuts <Stuart.Macdonald@student.uts.edu.au>
2023-10-13 18:07:48 +1100

    DH parameters

--- ZU3.m
+++ ZU3.m
@@ -10,8 +10,8 @@ classdef ZU3 < handle
         end
 
         function SetZU3(self)
-            L(1) = Link('d',0.1506,'a',-0.4,'alpha',0,'offset',0,'qlim',[deg2rad(-270) deg2rad(270)]);
-            L(2) = Link('d',-0.1150,'a',0.0,'alpha',deg2rad(-90),'offset',0,'qlim',[deg2rad(-85) deg2rad(265)]);
+            L(1) = Link('d',0.1506,'a',0.0,'alpha',deg2rad(90),'offset',0,'qlim',[deg2rad(-270) deg2rad(270)]);
+            L(2) = Link('d',-0.1150,'a',-0.4,'alpha',0,'offset',0,'qlim',[deg2rad(-85) deg2rad(265)]);
             L(3) = Link('d',0.1095,'a',0.2460,'alpha',pi/2,'offset',0,'qlim',[deg2rad(-175) deg2rad(175)]);
             L(4) = Link('d',-0.1175,'a',0.2280,'alpha',deg2rad(-90),'offset',0,'qlim',[deg2rad(-85) deg2rad(265)]);
             L(5) = Link('d',-0.1175,'a',0.0,'alpha',deg2rad(90),'offset',0,'qlim',[deg2rad(-270) deg2rad(270)]);
