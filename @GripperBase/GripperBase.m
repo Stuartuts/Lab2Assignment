47d632a544eb866be508bec7015811b8d826fa1c
quocminh-tran-1 <quocminh.tran-1@student.uts.edu.au>
2023-10-23 21:10:27 +1100

    Gripper Base and Hand
    Reload Gripper Base and Hand

---
+++ @GripperBase/GripperBase.m
@@ -0,0 +1,32 @@
+classdef GripperBase < RobotBaseClass
+    % LinearUR5 UR5 on a non-standard linear rail created by a student
+
+    properties(Access = public)
+        plyFileNameStem = 'GripperBase';
+
+    end
+
+    methods
+        % Define robot Function
+        function self = GripperBase(baseTr)
+%             axis([-1 1 -1 1 -1 1])
+%             hold on
+            self.CreateModel();
+            if nargin < 1
+                baseTr = eye(4);
+            end
+            self.model.base = self.model.base.T * baseTr;
+            self.model.plotopt = {'nojoints', 'noname', 'noshadow', 'nowrist','nobase','notiles','nojaxes'};
+            self.PlotAndColourRobot();
+
+        end
+
+        % Create the robot model
+        function CreateModel(self)
+            link(1) = Link('d',-0.0145,'a',0,'alpha',0,'qlim',0);
+            self.model = SerialLink(link,'name',self.name);
+
+        end
+
+    end
+end
