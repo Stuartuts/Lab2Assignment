26e863dde176a15dce790dddbd80ec28b3df7d7e
quocminh-tran-1 <quocminh.tran-1@student.uts.edu.au>
2023-10-26 22:31:55 +1100

    Voice and Video
    Source for the 1st video

--- untitled.m
+++ untitled.m
@@ -1,6 +1,15 @@
-axis([-2 2 -2 2 -2 2])
-hold on
-self.botshaker = PlaceObject('BotShaker1.ply',[0.5,0.6,0]);
-% self.botshaker_vert = get(self.botshaker,'Vertices');
-% self.botshaker_tr = [self.botshaker_vert,ones(size(self.botshaker_vert,1),1)]*transl(0.5,0.6,0)'';
-% set(self.botshaker_vert,'Vertices',self.botshaker_tr(:,1:3));
+hold on;
+Arm{1} = JAKAZU3;
+botShakerHandle = PlaceObject('BotShaker.ply',[0 0 0]);
+get(botShakerHandle,'Vertices');
+% Define the new position (x, y, z)
+newPosition = [1, 1, 0];
+Arm{1}.model.animate([0 0 0 0 0 deg2rad(-90)]);
+% Use set to update the 'Vertices' property with the new position
+y = trotx(pi/2)
+z = troty(-pi/2)
+endEffectorT = Arm{1}.model.fkine(Arm{1}.model.getpos())
+endRotation = [endEffectorT.n endEffectorT.o endEffectorT.a]
+set(botShakerHandle, 'Vertices', (endRotation * y(1:3,1:3)* z(1:3,1:3)* get(botShakerHandle, 'Vertices')')');
+set(botShakerHandle, 'Vertices', get(botShakerHandle, 'Vertices') + Arm{1}.model.fkine(Arm{1}.model.getpos()).t');
+ 
